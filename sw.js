8c14f9b528235211138084df0e579f801cfc2a2b
raghavkroynkr <raghavkroynkr@gmail.com>
2026-04-26 23:06:58 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'v3.5-pro-cache';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
